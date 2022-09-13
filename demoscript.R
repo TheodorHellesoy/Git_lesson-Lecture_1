09027606b8412b8dd28a8d81f0b3941c8b523cdb
Theodor Hellesøy <theohellesoy@gmail.com>
2022-09-13 09:11:55 +0200

    Adding crazy stuff

--- demoscript.R
+++ demoscript.R
@@ -1,8 +1,11 @@
 ## This is a test-script for lecture 1 in BAN400 ## 
 
-x <- rnorm(100)
-y <- rnorm(100)
+x <- rnorm(200)
+y <- rnorm(300)
 
 # Important feature
 
 # New feature
+
+
+# Crazy stuff
